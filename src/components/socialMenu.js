131dd2153f31877fbac8cd17376c3fe02b65486b
Lilaro <larochelle.lisa@gmail.com>
2020-02-15 17:21:34 -0500

    add SocialMenu component

---
+++ src/components/socialMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SocialMenu = () => {
+    return (
+        <div className="socialMenu">
+            Social Menu
+        </div>
+    )
+}
+
+export default SocialMenu
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import React from "react"
 import '../styles/styles.scss'
 import Header from "../components/header"
 import TitleCard from "../components/titleCard"
+import SocialMenu from "../components/socialMenu"
 
 
 const IndexPage = () => (
   <>
   <div className="container">
   <Header />
-  <TitleCard></TitleCard>
+  <SocialMenu/>
+  <TitleCard/>
   </div>
   </>
 )
