ea8d79a63f9c753936ed598af2ff63ae9eff9c1b
Lilaro <larochelle.lisa@gmail.com>
2020-02-15 21:30:13 -0500

    fb and ig icons/links

--- src/components/socialMenu.js
+++ src/components/socialMenu.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import igIcon from '../images/instagram.png'
+import fbIcon from '../images/facebook.png'
 
 const SocialMenu = () => {
     return (
         <div className="socialMenu">
-            Social Menu
+            <a href="https://www.instagram.com/blueshadescoffeewaffles/">
+               <img src={igIcon} alt="instagram icon"/>
+            </a>
+            <a href="https://www.facebook.com/blueshadescoffee/">
+                <img src={fbIcon} alt="facebook icon"/>
+            </a>
         </div>
     )
 }
