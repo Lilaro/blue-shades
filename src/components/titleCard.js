aaf8428ea40008806a837524eedf4a8de42ab3ee
Lilaro <larochelle.lisa@gmail.com>
2020-02-16 21:15:46 -0500

    new blueShade color

--- src/components/titleCard.js
+++ src/components/titleCard.js
@@ -3,7 +3,31 @@ import React from 'react'
 const TitleCard = () => {
     return (
         <div className="titleCard">
-           Blue Shades 
+            <div className="home-content">
+                {/* <h1>Blue Shades</h1> */}
+                <img src="../images/waffle_neon.svg"></img>
+                <p>
+                    A single-origin coffee and liege waffle
+                    shop in Worcester. With street snacks from 
+                    around the world!
+                </p>
+                <p>
+                    Come on in. 
+                    Our hours
+                    Monday-Saturday (except Tuesday) 8:30AM-7:00PM
+                    Sunday 10:30AM-3:00PM
+                    Closed Tuesdays
+                </p>
+                <p>
+                    256 Park Avenue
+                    Worcester, Massachusetts 01609
+                    United States
+                </p>
+                <p>
+                    (774) 420-2718
+                    Email: yumefoods@gmail.com
+                </p>
+            </div>
         </div>
     )
 }
